c10b1fa66681a1cef1cbe8e5342ddff114498322
=Angga161002 <=syahbandarpujangga16@gmail.com>
2022-11-29 01:44:24 +0700

    Slicing Halaman Login dan Register

--- src/components/login/RegisterPage2.jsx
+++ src/components/login/RegisterPage2.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Fligh from '../../assets/homepage/flight.webp';
 
-export default function RegisterPage() {
+export default function RegisterPage2() {
     return (
         <section className="container login">
             <div className="login-detail2">
@@ -10,46 +10,36 @@ export default function RegisterPage() {
                     <div className='name'>
                         <div className='row'>
                             <div className='col-md-6'>
-                                <label htmlFor="" className='mb-2'>First Name</label>
+                                <label htmlFor="" className='mb-2'>Tanggal Lahir</label>
                             </div>
                             <div className='col-md-6'>
                                 <input type="text" className='mb-3'/>
                             </div>
                             <div className='col-md-6'>
-                                <label htmlFor="" className='mb-2'>Last Name</label>
+                                <label htmlFor="" className='mb-2'>Photo Profil</label>
                             </div>
-                            <div className='col-md-6'>
-                                <input type="text" className='mb-3' />
-                            </div>
-                        </div>
-                    </div>
-                    <div className='name'>
-                        <div className='row'>
-                            <div className='col-md-6'>
-                                <label htmlFor="" className='mb-2'>Email</label>
-                            </div>
-                            <div className='col-md-6'>
-                                <input type="text" className='mb-3' />
+                            <div className='poto col-md-6'>
+                                <input type="file" className='mb-3' />
                             </div>
                         </div>
                     </div>
                     <div className='name'>
                         <div className='row'>
                             <div className='col-md-6'>
-                                <label htmlFor="" className='mb-2'>No Hp</label>
+                                <label htmlFor="" className='mb-2'>Password</label>
                             </div>
                             <div className='col-md-6'>
                                 <input type="text" className='mb-3' />
                             </div>
                             <div className='col-md-6'>
-                                <label htmlFor="" className='mb-2'>Alamat</label>
+                                <label htmlFor="" className='mb-2'>Password Confirm</label>
                             </div>
                             <div className='col-md-6'>
                                 <input type="text" className='mb-3' />
                             </div>
                         </div>
                     </div>
-                    <a href="/register2"><button type="button" className="btn btn-primary">Next</button></a>
+                    <button type="button" className="btn btn-primary">Create Your Account</button>
                 </form>
             </div>
             <img src={Fligh} alt="" className="fligh" />
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes as Switch, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
+import Register2 from "../pages/Register2";
 
 
 export default function Routes() {
@@ -12,6 +13,7 @@ export default function Routes() {
                 <Route exact path="/" element={<Home />} />
                 <Route exact path="/login" element={<Login />} />
                 <Route exact path="/register" element={<Register />} />
+                <Route exact path="/register2" element={<Register2 />} />
             </Switch>
         </BrowserRouter>
     )
