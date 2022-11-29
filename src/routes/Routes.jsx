c24c8b5044da7015028a3be23a147fb68c54da9a
Pahrurozi <61068212+pahrurozidev@users.noreply.github.com>
2022-11-29 17:59:46 +0800

    feature/slicing-dashboard-admin

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,10 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes as Switch, Route } from "react-router-dom";
 import Home from "../pages/Home";
-import Login from "../pages/Login";
-import Register from "../pages/Register";
-import Register2 from "../pages/Register2";
-
 import DashboardAdmin from "../pages/DashboardAdmin";
 
 export default function Routes() {
