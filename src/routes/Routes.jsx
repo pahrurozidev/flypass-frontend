5b9f09bfc98be7e6680b2bed0815a3e3ebed7b9a
Pahrurozi <61068212+pahrurozidev@users.noreply.github.com>
2022-11-29 17:31:15 +0800

    feature/slicing-dashboard-admin

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,16 +1,31 @@
 import React from "react";
 import { BrowserRouter, Routes as Switch, Route } from "react-router-dom";
 import Home from "../pages/Home";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import Register2 from "../pages/Register2";
+
 import DashboardAdmin from "../pages/DashboardAdmin";
 
 export default function Routes() {
     return (
         <BrowserRouter>
             <Switch>
+                {/* homepage */}
                 <Route exact path="/" element={<Home />} />
+
+                {/* auth */}
+                <Route exact path="/login" element={<Login />} />
+                <Route exact path="/register" element={<Register />} />
+                <Route exact path="/register2" element={<Register2 />} />
+
+                {/* admin */}
                 <Route path="/dashboard" element={<DashboardAdmin />} />
                 <Route path="/customers" element={<DashboardAdmin />} />
+                <Route path="/ticket" element={<DashboardAdmin />} />
                 <Route path="/flight" element={<DashboardAdmin />} />
+
+                {/* user */}
             </Switch>
         </BrowserRouter>
     )
