3a8b48cfe3ed266a77eccc94c5a273c6545cccd1
haikalarif <haikal.arif164@gmail.com>
2022-11-29 19:10:44 +0700

    feature/slicing-dashboard-admin

---
+++ src/assets/sidebarLink.js
@@ -0,0 +1,24 @@
+const sidebarLink = [
+    {
+        path: '/dashboard',
+        icon: 'bi bi-house-door',
+        display: 'Dashboard',
+    },
+    {
+        path: '/customers',
+        icon: 'bi bi-people',
+        display: 'Customers',
+    },
+    {
+        path: '/ticket',
+        icon: 'bi bi-ticket-perforated',
+        display: 'Ticket',
+    },
+    {
+        path: '/flight',
+        icon: 'bi bi-airplane',
+        display: 'Flight',
+    },
+];
+
+export default sidebarLink;
