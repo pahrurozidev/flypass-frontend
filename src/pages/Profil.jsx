9397154918730e57eb54e0288d1357ad3686295d
=Angga161002 <=syahbandarpujangga16@gmail.com>
2022-12-06 14:53:02 +0700

    Slicing halaman dashboard user, profil menu

--- src/pages/Profil.jsx
+++ src/pages/Profil.jsx
@@ -1,6 +1,8 @@
 import React, { Component, Fragment } from "react";
 import { connect } from "react-redux";
-import DashboardPage from '../components/customer/DashboardPage';
+import Navbar from '../components/customer/dashboard/navbar/navbar';
+import Sidebar from '../components/customer/dashboard/sidebar/Sidebar';
+import ProfilPage from '../components/customer/ProfilPage';
 import { actionType } from '../redux/reducer/globalActionType';
 
 // style homepage
@@ -8,11 +10,19 @@ import '../styles/homepage.css'
 import '../styles/login.css'
 import '../styles/dashboard.css'
 
-class Dashboard extends Component {
+class Profil extends Component {
     render() {
         return (
             <Fragment>
-                <DashboardPage />
+                <div className='d-flex'>
+                    <Sidebar />
+                    <div id='content-wrapper' className='d-flex flex-column'>
+                        <div id="content">
+                            <Navbar />
+                            <ProfilPage />
+                        </div>
+                    </div>
+                </div>
             </Fragment>
         )
     }
@@ -35,4 +45,4 @@ const mapDispatchToProps = (dispatch) => {
     }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Dashboard);
+export default connect(mapStateToProps, mapDispatchToProps)(Profil);
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import { BrowserRouter, Routes as Switch, Route } from "react-router-dom";
 import Home from "../pages/Home";
-import Dashboard from "../pages/Dashboard";
+import Profil from "../pages/Profil";
+import EditProfil from "../pages/EditProfil";
 
 
 export default function Routes() {
     return (
         <BrowserRouter>
             <Switch>
-                <Route exact path="/" element={<Home />} />
-                <Route exact path="/dashboarduser" element={<Dashboard />} />   
+                <Route exact path="/" element={<Home />} />  
+                <Route exact path="/profil" element={<Profil />} />
+                <Route exact path="/editprofil" element={<EditProfil />} />
             </Switch>
         </BrowserRouter>
     )
