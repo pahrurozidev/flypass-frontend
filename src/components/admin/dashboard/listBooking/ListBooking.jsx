6c9ee8dec6a554070d9d07b540455462737fa863
Pahrurozi <61068212+pahrurozidev@users.noreply.github.com>
2022-12-08 09:50:15 +0800

    fix responsive list booking menu

--- src/components/admin/dashboard/listBooking/ListBooking.jsx
+++ src/components/admin/dashboard/listBooking/ListBooking.jsx
@@ -12,67 +12,69 @@ export default function ListBooking() {
                 <a className="btn btn-date"><span>Date Range</span></a>
             </div>
             <div className="container list-booking p-0">
-                <table className="table">
-                    <thead>
-                        <tr>
-                            <th>Airlines</th>
-                            <th>From</th>
-                            <th> </th>
-                            <th>To</th>
-                            <th>Duration</th>
-                            <th>Date</th>
-                            <th>People</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td data-title="Airlines" className="airlines">
-                                <img src={Garuda} alt="Airplanes" /><br className="d-none" />
-                                <span>Garuda Indonesia</span>
-                            </td>
-                            <td data-title="From" className="departure-from">
-                                <p className="departure-time">08:05</p>
-                                <span className="from-flight">Jakarta</span>
-                            </td>
-                            <td className="line-flight">
-                                <img src={Line} alt="" />
-                            </td>
-                            <td data-title="To" className="arrival-to">
-                                <p className="arrival-time">09:50</p>
-                                <span className="to-flight">Singapore</span>
-                            </td>
-                            <td data-title="Duration" className="duration">
-                                <p className="duration-flight">1h 45m</p>
-                                <span className="type-direct">Direct</span>
-                            </td>
-                            <td data-title="Date" className="departure-date">01 Dec 2022</td>
-                            <td data-title="People" className="people">5</td>
-                        </tr>
-                        <tr>
-                            <td data-title="Airlines" className="airlines">
-                                <img src={Garuda} alt="Airplanes" /><br className="d-none" />
-                                <span>Garuda Indonesia</span>
-                            </td>
-                            <td data-title="From" className="departure-from">
-                                <p className="departure-time">08:05</p>
-                                <span className="from-flight">Jakarta</span>
-                            </td>
-                            <td className="line-flight">
-                                <img src={Line} alt="" />
-                            </td>
-                            <td data-title="To" className="arrival-to">
-                                <p className="arrival-time">09:50</p>
-                                <span className="to-flight">Singapore</span>
-                            </td>
-                            <td data-title="Duration" className="duration">
-                                <p className="duration-flight">1h 45m</p>
-                                <span className="type-direct">Direct</span>
-                            </td>
-                            <td data-title="Date" className="departure-date">01 Dec 2022</td>
-                            <td data-title="People" className="people">5</td>
-                        </tr>
-                    </tbody>
-                </table>
+                <div className="booking-table">
+                    <table className="table">
+                        <thead>
+                            <tr>
+                                <th>Airlines</th>
+                                <th>From</th>
+                                <th> </th>
+                                <th>To</th>
+                                <th>Duration</th>
+                                <th>Date</th>
+                                <th>People</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td data-title="Airlines" className="airlines">
+                                    <img src={Garuda} alt="Airplanes" /><br className="d-none" />
+                                    <span>Garuda Indonesia</span>
+                                </td>
+                                <td data-title="From" className="departure-from">
+                                    <p className="departure-time">08:05</p>
+                                    <span className="from-flight">Jakarta</span>
+                                </td>
+                                <td className="line-flight">
+                                    <img src={Line} alt="" />
+                                </td>
+                                <td data-title="To" className="arrival-to">
+                                    <p className="arrival-time">09:50</p>
+                                    <span className="to-flight">Singapore</span>
+                                </td>
+                                <td data-title="Duration" className="duration">
+                                    <p className="duration-flight">1h 45m</p>
+                                    <span className="type-direct">Direct</span>
+                                </td>
+                                <td data-title="Date" className="departure-date">01 Dec 2022</td>
+                                <td data-title="People" className="people">5</td>
+                            </tr>
+                            <tr>
+                                <td data-title="Airlines" className="airlines">
+                                    <img src={Garuda} alt="Airplanes" /><br className="d-none" />
+                                    <span>Garuda Indonesia</span>
+                                </td>
+                                <td data-title="From" className="departure-from">
+                                    <p className="departure-time">08:05</p>
+                                    <span className="from-flight">Jakarta</span>
+                                </td>
+                                <td className="line-flight">
+                                    <img src={Line} alt="" />
+                                </td>
+                                <td data-title="To" className="arrival-to">
+                                    <p className="arrival-time">09:50</p>
+                                    <span className="to-flight">Singapore</span>
+                                </td>
+                                <td data-title="Duration" className="duration">
+                                    <p className="duration-flight">1h 45m</p>
+                                    <span className="type-direct">Direct</span>
+                                </td>
+                                <td data-title="Date" className="departure-date">01 Dec 2022</td>
+                                <td data-title="People" className="people">5</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </>
     );
