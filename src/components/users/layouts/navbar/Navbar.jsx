4866d7021d40c67ff0309ddc55ce4a29495ffec6
=Angga161002 <=syahbandarpujangga16@gmail.com>
2022-12-22 14:53:20 +0700

    revisi personal-info-integrate 2.1

--- src/components/users/layouts/navbar/Navbar.jsx
+++ src/components/users/layouts/navbar/Navbar.jsx
@@ -1,9 +1,13 @@
 import React, { Component, Fragment, useState, useEffect } from "react";
+import { connect } from "react-redux";
 import axios from "axios";
 import jwt_decode from "jwt-decode";
 import { useHistory } from 'react-router-use-history';
 import { Link } from 'react-router-dom';
-import { Menu } from 'react-feather';
+import { Menu } from "react-feather";
+import { Search, Bell } from "react-feather";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import Profile from '../../../../assets/dasboard-admin/profile.svg';
 import { actionType } from '../../../../redux/reducer/globalActionType';
 
@@ -31,26 +35,29 @@ export default function navbar() {
       }, []);
     return (
         <nav className='navbar navbar-expand topbar static-top card fixed-top border-end-0'>
-            <div></div>
-            <ul className='navbar-nav profile-dropdown ms-auto'>
-                <li className="nav-item dropdown user-profile">
-                    <a href="#" className='nav-link dropdown-toggle link-profile' id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        <img className='img-profile rounded-circle' src={Profile} alt="" />
-                        <span className='d-none d-lg-inline text-name'>Hi, {name}</span>
-                    </a>
-                    <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <Link to={'/'} className="dropdown-item">Beranda</Link>
-                        <li>
-                            <hr className="dropdown-divider" />
-                        </li>
-                        <li>
-                            <a className="dropdown-item" href="#">Logout</a>
-                        </li>
-                    </ul>
-                </li>
-            </ul>
-        </nav>
+                <ul className='navbar-nav profile-dropdown ms-auto'>
+                    {/* <li className='nav-item sidebar-toggle'>
+                        <Menu
+                            size={38}
+                            onClick={this.props.showSidebarDispatch}
+                            className="bg-light px-2 rounded-circle shadow border text-secondary" />
+                    </li> */}
+                    <li className="nav-item dropdown user-profile">
+                        <a href="#" className='nav-link dropdown-toggle link-profile' id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <img className='img-profile rounded-circle' src={Profile} alt="" />
+                            <span className='d-none d-lg-inline text-name'>Hi, {name}</span>
+                        </a>
+                        <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <Link to={'/'} className="dropdown-item">Beranda</Link>
+                            <li>
+                                <hr className="dropdown-divider" />
+                            </li>
+                            <li>
+                                <a className="dropdown-item" href="#">Logout</a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </nav>
     )
 }
-
-
