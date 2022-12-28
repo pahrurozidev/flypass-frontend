50a0bbb55c2da3958279e1a879be223db7965dff
=Angga161002 <=syahbandarpujangga16@gmail.com>
2022-12-28 16:55:07 +0700

    dashboard-user-all-menu-revisi2

--- src/components/customer/ProfilPage.jsx
+++ src/components/customer/ProfilPage.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import jwt_decode from "jwt-decode";
 import { useHistory } from 'react-router-use-history';
 import { Link, Navigate } from 'react-router-dom';
-import UserCircle from '../../assets/homepage/user-circle.png';
+import Profile from '../../assets/dasboard-admin/profile.svg';
 import ArrowLeft from '../../assets/homepage/arrow-left.png';
 import { ArrowCircleLeft2 } from 'iconsax-react';
 
@@ -56,9 +56,9 @@ export default function ProfilPage() {
               <div className='label'>Profile</div>
             </Link>
           </div>
-          <section className="container login ">
+          {/* <section className="container login ">
             <div className="login-detail">
-              <img src={image} class="nav-link user-image" alt="UserCircle" />
+              <img src={UserCircle} class="nav-link user-image" />
               <h6>User Name :</h6>
               <p>{name}</p>
               <h6>Date Of Birth :</h6>
@@ -71,7 +71,76 @@ export default function ProfilPage() {
               <p>{gender}</p>
               <a href="/#/user/dashboard/editprofile"><button type="button" className="btn btn-primary">Edit Profile</button></a>
             </div>
-          </section>
+          </section> */}
+          <div className="card mt-3 p-2">
+                        <div className="admin-customer-detail d-flex flex-column gap-2">
+                            <section className='card p-3'>
+                                <h5 className='border-bottom pb-2'>Personal</h5>
+                                <img src={Profile} alt="" className='mt-3 mb-4' width={70} />
+                                <div className='data-list'>
+                                    <div className='col-12 d-flex flex-column gap-3 gap-md-0'>
+                                        <div className='data-item'>
+                                            <p className='data-item__name'>First Name</p>
+                                            <p className='data-item__value'>: {name}</p>
+                                        </div>
+                                        <div className='data-item d-md-none'>
+                                            <p className='data-item__name'>Last Name</p>
+                                            <p className='data-item__value'>: {name}</p>
+                                        </div>
+                                        <div className='data-item'>
+                                            <p className='data-item__name'>Date of Birth</p>
+                                            <p className='data-item__value'>: {birthDate}</p>
+                                        </div>
+                                        <div className='data-item'>
+                                            <p className='data-item__name'>Date of Birth</p>
+                                            <p className='data-item__value'>: {birthDate}</p>
+                                        </div>
+                                        <div className='data-item'>
+                                            <p className='data-item__name'>Phone</p>
+                                            <p className='data-item__value'>: +6285xxxxxx</p>
+                                        </div>
+                                        <div className='data-item'>
+                                            <p className='data-item__name'>Pos Code</p>
+                                            <p className='data-item__value'>: {phone}</p>
+                                        </div>
+                                        <div className='data-item'>
+                                            <p className='data-item__name'>Address</p>
+                                            <p className='data-item__value'>: Jln xxx perigi city space</p>
+                                        </div>
+                                    </div>
+                                    <div className='col-12 d-flex flex-column gap-3 gap-md-0'>
+                                        <div className='data-item'>
+                                            <p className='data-item__name'>Last Name</p>
+                                            <p className='data-item__value'>: {name}</p>
+                                        </div>
+                                        <div className='data-item'>
+                                            <p className='data-item__name'>Gender</p>
+                                            <p className='data-item__value'>: {gender}</p>
+                                        </div>
+                                        <div className='data-item'>
+                                            <p className='data-item__name'>Email</p>
+                                            <p className='data-item__value'>: {email}</p>
+                                        </div>
+                                        <div className='data-item'>
+                                            <p className='data-item__name'>City</p>
+                                            <p className='data-item__value'>: Mataram</p>
+                                        </div>
+                                        <div className='data-item'>
+                                            <p className='data-item__name'>Nationality</p>
+                                            <p className='data-item__value'>: Indonesia</p>
+                                        </div>
+                                        <div className='data-item'>
+                                          <a href="/#/user/dashboard/editprofile">
+                                            <div className='btn btn-primary crud-flight-btn d-flex gap-1 justify-content-center shadow' style={{width:"100px", marginLeft:"-2px"}}>
+                                              <span>Edit Data</span>
+                                            </div>
+                                          </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </section>
+                        </div>
+                    </div>
         </div>
       </div>
     </div>
