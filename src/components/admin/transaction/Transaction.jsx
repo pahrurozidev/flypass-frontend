16c22bc2aa19dd9da3940327ef89abefbdb459db
Pahrurozi <61068212+pahrurozidev@users.noreply.github.com>
2023-01-02 15:45:19 +0800

    fix transaction navbar admin

--- src/components/admin/transaction/Transaction.jsx
+++ src/components/admin/transaction/Transaction.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import Sidebar from '../layouts/sidebar/Sidebar';
-// import Navbar from '../layouts/navbar/Navbar';
+import Navbar from '../layouts/navbar/Navbar';
 import TransactionList from './TransactionList';
 import TransactionDetail from './TransactionDetail';
 
-export default function Transaction({ showSidebarDispatch }) {
+export default function Transaction() {
     const { id } = useParams();
 
     return (
         <div>
             <Sidebar />
             <div>
-                {/* <Navbar /> */}
+                <Navbar />
                 <div className="main-container">
                     {id && <TransactionDetail />}
                     {!id && <TransactionList />}
